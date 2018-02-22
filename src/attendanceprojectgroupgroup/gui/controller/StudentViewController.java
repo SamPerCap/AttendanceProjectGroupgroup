205ff7673dfe54258cad6ce02beee1dafa1bf433
SamPerCaparros <samu1667@easv365.dk>
2018-02-22 11:15:40 +0100

    StudentView nearly full resizable

--- src/attendanceprojectgroupgroup/gui/controller/StudentViewController.java
+++ src/attendanceprojectgroupgroup/gui/controller/StudentViewController.java
@@ -7,7 +7,12 @@ package attendanceprojectgroupgroup.gui.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,9 +21,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import static javax.management.Query.value;
 
 /**
  * FXML Controller class
@@ -26,11 +34,11 @@ import javafx.stage.Stage;
  * @author Samuel
  */
 public class StudentViewController implements Initializable {
-
+    
     @FXML
     private Label idLabel;
     @FXML
-    private TableColumn<?, ?> columnWeek;
+    private TableColumn<Integer, Integer> columnWeek;
     @FXML
     private TableColumn<?, ?> columnMonday;
     @FXML
@@ -44,8 +52,8 @@ public class StudentViewController implements Initializable {
     @FXML
     private TableColumn<?, ?> columnWeekTotal;
     @FXML
-    private TableView<?> weekTableView;
-
+    private TableView<Integer> weekTableView;
+    
     @FXML
     private Button btnSeeDetailsChart;
     @FXML
@@ -59,18 +67,18 @@ public class StudentViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-
+        fillintList();
     }
-
+    
     @FXML
     private void clickSeeDetailsChart(ActionEvent event) throws IOException {
         Stage stage = new Stage();
 
         // stage.initModality(Modality.APPLICATION_MODAL);
         FXMLLoader fxLoader = new FXMLLoader(getClass().getResource("/attendanceprojectgroupgroup/gui/view/DetailedChartStudent.fxml"));
-
+        
         Parent root = fxLoader.load();
-
+        
         Scene scene = new Scene(root);
         stage.setTitle("BarChart");
         stage.setScene(scene);
@@ -78,11 +86,30 @@ public class StudentViewController implements Initializable {
         //doesn't work
         Stage window = (Stage) btnSeeDetailsChart.getScene().getWindow();
         stage.close();
-
+        
     }
-
+    
+    List<Integer> intList = new ArrayList<>();
+    
+    private void fillintList() {
+        
+        for (int i = 1; i < 6; i++) {
+            intList.add(i);
+        }
+        
+    }
+    
+    private void fillTheColumn() {
+        
+        Callback<TableColumn<Integer, Integer>, TableCell<Integer, Integer>> value = null;
+        for (int i = 0; i < intList.size(); i++) {
+        }
+        
+        columnWeek.setCellFactory(value);
+        
+    }
     private LogInViewController parent;
-
+    
     public void setParentWindowController(LogInViewController parent) {
         this.parent = parent;
     }
